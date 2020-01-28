fa0dfee39060651f96f331072d25db6f99490f56
Isabella Bitencourt <isabella.bitenocurt@accenture.com>
2020-01-28 14:39:58 -0300

    Creating project

---
+++ QuizApp/QuizApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by isabella.bitencourt on 28/01/20.
+//  Copyright © 2020 isabella.bitencourt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
